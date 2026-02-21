c11ce5c764f8a5eab4927160a0bf6b77ff49b0f9
Vantake <letuzinukure67@gmail.com>
2026-02-21 23:39:41 +0700

    Global SWR configuration and data caching provider

--- components/providers/swr-provider.tsx
+++ components/providers/swr-provider.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react'
 
 const fetcher = async (url: string) => {
   const res = await fetch(url)
-  if (!res.ok) throw new Error('Failed to fetch')
+  if (!res.ok) throw new Error('Failed to fetch') 
   return res.json()
 }
 
